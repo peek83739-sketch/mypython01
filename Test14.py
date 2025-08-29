93447b3107205c91c8c5c7eaabbb1f61e251d0e4
chanakan6519410016 <s6519410016@sau.ac.th>
2025-08-29 11:41:01 +0700

    Do control selection Complete

---
+++ Test14.py
@@ -0,0 +1,19 @@
+# if-elif-else
+
+score = 78
+
+if score >= 80 :
+    print('Grade A')
+if score >= 70 :
+    print('Grade B')
+if score >= 60 :
+    print('Grade C')
+if score >= 50 :
+    print('Grade D')
+else : #colon อย่าลืม *** else สุดท้ายจะเป็น elif ก็ได้
+    print('Grade F')
+
+print('Ei ei.....')
+print('Ei ei.....')
+
+
